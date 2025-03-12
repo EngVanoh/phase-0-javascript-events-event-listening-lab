51db5c41797ab99b94267bb3545679311cee5ce8
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-03-12 11:29:07 +0300

    eventlisten

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    // Select the input element with id="button"
+    const input = document.getElementById("button");
+  
+    // Add a click event listener to the input element
+    input.addEventListener("click", function () {
+      alert("I was clicked!");
+    });
+  }
+  
+  // Call the function to activate the event listener
+  addingEventListener();
